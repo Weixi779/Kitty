197a3f73ceb2ef1a273aeb056c24785f0311a3db
weixi <sunshiwei418@126.com>
2025-08-12 21:50:10 +0800

    feat: add xcode project the fisrt commit

---
+++ Kitty/Kitty/KittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittyApp.swift
+//  Kitty
+//
+//  Created by 孙世伟 on 2025/8/12.
+//
+
+import SwiftUI
+
+@main
+struct KittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kitty/KittyTests/KittyTests.swift
@@ -0,0 +1,17 @@
+//
+//  KittyTests.swift
+//  KittyTests
+//
+//  Created by 孙世伟 on 2025/8/12.
+//
+
+import Testing
+@testable import Kitty
+
+struct KittyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
